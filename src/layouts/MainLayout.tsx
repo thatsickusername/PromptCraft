971d7ac2dbc90ecbeabd7f4904f10545385bf846
thatsickusername <manish1234pandeycrpf@gmail.com>
2025-08-24 17:49:35 +0530

    fix: react declared never used

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
 import SideNavbar from "../components/sidenav/SideNavbar";
 import CustomAlert from "../components/ui/CustomAlert";
